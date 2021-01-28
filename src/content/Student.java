698e90d6fb04ee5f829cc68c58a60689b5778727
Han Byul Moon <Han Byul Moon@192.168.0.66>
2021-01-28 11:38:55 -0500

    First Commit

---
+++ src/content/Student.java
@@ -0,0 +1,28 @@
+/*
+ * Han Byul Moon
+ * Student ID: 991 624 436
+ */
+package content;
+
+
+public class Student {
+    
+    //----- Data Fields -----
+    private String name; 
+    
+    //----- Constructors -----
+    public Student(String name) {
+        this.name = name; 
+    }
+    
+    //----- Get Methods -----
+    public String getName() {
+        return this.name; 
+    }
+    
+    //----- Set Methods -----
+    public void setName(String name) {
+        this.name = name; 
+    }
+    
+}
---
+++ src/studentlist/Main.java
@@ -0,0 +1,39 @@
+/*
+ * Han Byul Moon
+ * Student ID: 991 624 436
+ */
+package studentlist;
+
+import java.util.Scanner; 
+import content.Student; 
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+        
+        Student[] studentList = new Student[4]; 
+        
+        Scanner input = new Scanner(System.in); 
+        
+        for (int i = 0; i < studentList.length; i++) {
+            
+            System.out.print("Enter the student's name: ");
+            String name = input.nextLine(); 
+            Student student = new Student(name); 
+            
+            studentList[i] = student; 
+            //Han, Bill, Kris, Dino
+        }
+        
+        System.out.println("\nPrinting the students:");
+        
+        String format = "The student's name is %s\n"; 
+        
+        for (Student student : studentList) {
+            System.out.printf(format, student.getName()); 
+        }
+        
+    }
+    
+}
